1425a411f7c1269eec81d132621406d325653959
Thomas Nguyen <tttnguyen729@gmail.com>
2020-04-27 20:18:35 -0500

    first commit

---
+++ init.el
@@ -0,0 +1,21 @@
+
+
+(package-initialize) ;; Added by Package.el
+
+(scroll-bar-mode -1) ;; removes the scroll bar
+
+(setq visible-bell 1 ;; gets rid of noise
+      display-line-numbers t) ;; displays line numbers
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (auctex org))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
