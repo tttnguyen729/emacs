933beb83b34190a44fb67d3b52e1c1942b5cfea2
Thomas Nguyen <tttnguyen729@gmail.com>
2020-05-02 21:28:20 -0500

    hello

--- init.el
+++ init.el
@@ -1,20 +1,35 @@
 
-(package-initialize) ;; Added by Package.el
-
-(scroll-bar-mode -1) ;; removes the scroll bar
-
-(setq visible-bell 1 ;; gets rid of noise
-      display-line-numbers t) ;; displays line numbers
 
 (setq inhibit-startup-message t) ;; gets rid of splash screen
 (find-file "~/.emacs.d/main.org")
+
+(package-initialize) ;; Added by Package.el
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(load-theme 'tango-dark)
+
+(scroll-bar-mode -1) ;; removes the scroll bar
+(tool-bar-mode -1) ;; removes tool bar
+(menu-bar-mode -1) ;; removes menu bar
+
+(setq visible-bell 1) ;; gets rid of noise
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; for org mode
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (auctex org))))
+ '(org-agenda-files (quote ("~/Documents/life.org")))
+ '(package-selected-packages (quote (company auctex org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
