2a245a1051faccd8b13b2696f984ec6be801f101
Thomas Nguyen <tttnguyen729@github.com>
2020-12-16 17:36:50 -0500

    Testing out magit

---
+++ elpa/dash-20201215.40/dash-pkg.el
@@ -0,0 +1,9 @@
+(define-package "dash" "20201215.40" "A modern list library for Emacs" 'nil :commit "e4e52691c5130336c5675f697d09e468a9ddd222" :keywords
+  ("extensions" "lisp")
+  :authors
+  (("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainer
+  ("Magnar Sveen" . "magnars@gmail.com"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- init.el
+++ init.el
@@ -1,5 +1,9 @@
+;; Melpa Support
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
 ;; Decluttering appearance
-(tool-bar-mode 1)
+(tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
@@ -17,3 +21,28 @@
 
 ;; Turning off error beeping
 (setq visible-bell t)
+
+;; Turning on line numbers
+(global-display-line-numbers-mode)
+
+;; Org mode
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+;; Move file backups to a dedicated directory
+(setq backup-directory-alist `(("." . "~/.saves")))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
